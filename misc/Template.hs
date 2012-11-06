1b607f07da51707692b0d5439852cf0a7d1c7132
Hans Höglund <hans@hanshoglund.se>
2012-11-06 18:38:57 +0100

    - Added template

---
+++ misc/Template.hs
@@ -0,0 +1,32 @@
+
+{-# LANGUAGE MagicHash, CPP, ForeignFunctionInterface, OverloadedStrings,
+    StandaloneDeriving, DeriveFunctor, DeriveFoldable, GeneralizedNewtypeDeriving,
+    NoMonomorphismRestriction, EmptyDataDecls #-}
+
+-------------------------------------------------------------------------------------
+
+-- |
+-- Module      : Web.FooBar
+-- Copyright   : (c) Hans Hoglund 2012
+--
+-- License     : BSD-style
+--
+-- Maintainer  : hans@hanshoglund.se
+-- Stability   : experimental
+-- Portability : Haste
+--
+-- Bindings for FooBar.
+--
+
+-------------------------------------------------------------------------------------
+
+module Web.FooBar -- (
+        -- ** Foo
+        -- ** Bar
+-- )
+where
+
+import Prelude -- hiding (take, drop, filter, length)
+import Foreign.JavaScript -- hiding (take, drop, slice, length)
+
+
